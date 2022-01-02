217a94c1521f2972deff36356fdfebdd8f6a54f2
Bahriddin1 <bahrid1777@outlook.com>
2022-01-02 18:27:14 +0900

    add...

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let container = document.querySelector(".menu-btn");
+let navActive = document.querySelector(".menu-nav");
+
 
 container.addEventListener("click", function(e){
   container.classList.toggle("menu-btn_active");
+  navActive.classList.toggle("menu-nav-active")
 });
